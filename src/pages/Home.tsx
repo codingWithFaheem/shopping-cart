2f7993834d4945d51d933038bc6c2f2937e1709e
codingWithFaheem <faheemk5914858@gmail.com>
2023-05-20 22:38:14 +0500

    completed home page with responsive mobile firendly

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,20 +10,23 @@ const Home =  () =>  {
   const {isCartOpen} = useShoppingCart()
   const cartState = isCartOpen ? 'blur-sm' :''
   return (
-    <div className={`bg-gradient-to-r from-[#65c498] to-[#7cd7ee]   h-[100vh] pt-20  ${cartState} `}>
-          <div className=' lg:w-[1200px] h-[63vh] w-full m-auto
-          flex
+    <div className={`bg-gradient-to-r from-[#65c498] to-[#7cd7ee]   h-[118vh] lg:h-screen pt-20  ${cartState} `}>
+          <div className=' lg:w-[1200px] h-full2
+          
+          lg:h-[63vh] w-full m-auto
+          flex flex-col items-center lg:flex-row
+          px-6
           '>
-              <div className=" px-6 w-[56%]
-                    flex flex-col items-start justify-start gap-y-10">
-                  <p className='leading-[5.5rem] font-bold lg:pr-8 lg:mt-12 heroFont text-[4.5rem] text-[#123]'>We Create home more asthetic</p>
+              <div className="w-[89%]
+                    flex flex-col  items-center  lg:items-start lg:justify-start gap-y-10">
+                  <p className=' leading-[4.2rem] lg:leading-[5.5rem] font-bold lg:pr-8 lg:mt-12 heroFont text-center lg:text-start  text-[3.5rem] lg:text-[4.5rem] text-[#123]'>We create home more asthetic</p>
                   <p className='  text-[18px] text-[#123] lg:w-[49%] pl-5'>Enjoy new Luxurious sitting chairs every time you move  and feel the castle house of  your dream</p>
-                      <button className='tracking-[0.2rem] w-[14rem] py-4 bg-[#123]  text-[#93edcf] ml-4 '><Link to='/store'>VISIT OUR STORE </Link></button>
+                      <button className='tracking-[0.2rem] w-[14rem] py-4 bg-[#123]  text-[#93edcf] lg:ml-4 '><Link to='/store'>VISIT OUR STORE </Link></button>
               </div>
-              <div className=" lg:relative">
-                      <div className="z-0  lg:ml-[7rem] mt-6  lg:w-[23rem] h-[28.5rem] bg-[#123] "></div>
-                      <img src="./imgs/heroImg.png" alt=""  className='z-10 object-contain h-[31rem] absolute top-0'/>
-                      <div className=" px-4 h-[11rem] w-[76%] ml-28
+               <div className="  relative  mt-8 md:mt-0">
+                      <div className="z-0 rounded-t-full md:rounded-t-[0] lg:ml-[7rem] lg:mt-6 w-[20rem] lg:w-[23rem] h-[29.5rem] bg-[#123] "></div>
+                      <img src="./imgs/heroImg.png" alt=""  className=' z-10 object-contain h-[24rem] lg:h-[31rem] absolute left-5 bottom-[3rem] lg:top-0'/>
+                     {/* <div className=" px-4 h-[11rem] w-[76%] ml-28
                       flex justify-between items-center
                       ">
                         <div className=" w-[7rem] h-[6rem] flex flex-col justify-between items-start">
@@ -41,9 +44,9 @@ const Home =  () =>  {
                           <p className="heroFont text-[25px] font-bold">162</p>
                           <p className='leading-6 font-bold text-[12px] text-[#123] product-fon'>Custamize theme </p>
                            </div>
-
-                      </div>
-              </div>
+                          
+                      </div>  */}
+               </div> 
           </div>
 
     </div>
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -4,14 +4,14 @@ import { useShoppingCart } from '../context/ShopingCartContext';
 
 
 const Store:FC = () =>  {
- const {storeItems , isCartOpen} = useShoppingCart()
+ const {storeItems , isCartOpen , closeCart} = useShoppingCart()
   const cartState = isCartOpen ? 'blur-sm' :''
   return (
     <div className={`${cartState} lg:w-[1240px] h-[120vh] 
      flex flex-wrap justify-center items-start gap-7 
      m-auto mt-[7rem] px-6 py-20 
       bg-gradient-to-r from-[#e4fdf2] to-[#e7e8e8]
-    `}>
+    `} onClick={closeCart}>
     {storeItems.map(item => { 
       return <div>
             <StoreItem  key={item.id} {...item} />
